dee870cca82cc3c87e0e52abf883dcdef40415d3
Shamim Shihab <shamimshihab56@gmail.com>
2024-06-06 20:44:42 -0700

    Time: 114 ms (45.03%), Space: 17.7 MB (94.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,17 +1,35 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+
+# usimg sum of two array
+
+        def sumNumber(number):
+            m= 0
+            n = 0
+            for i in range(len(number)):
+                m = m + number[i]
+                n = n + i
+            n = n + len(number)
+            return [m, n]
+        
+        i ,j =  sumNumber(nums)
+        
+        return j-i 
+        
+
+      
 # using xor
-     sum = 0
-     n = len(nums)
+    #  sum = 0
+    #  n = len(nums)
 
     
 
-     for i in range(len(nums)):
-        print(nums[i])
-        sum = sum ^ i ^ nums[i]
-     sum = n ^ sum
+    #  for i in range(len(nums)):
+    #     print(nums[i])
+    #     sum = sum ^ i ^ nums[i]
+    #  sum = n ^ sum
      
-     return sum
+    #  return sum
 #  sorting
         # nums.sort()
 
