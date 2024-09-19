7ebef92939ac0e9dd58124f9515fb17b1c1b7eb2
Shamim Shihab <shamimshihab56@gmail.com>
2024-09-19 15:46:19 -0700

    Time: 54 ms (9.14%), Space: 16.8 MB (18.00%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,15 +1,42 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
+    def findPeakElement(self, nums: List[int]) -> int: 
+        l, r = 0 , len(nums) -1
+
+        while l<=r:
+
+            m = l + ((r-l)//2)
+
+
+            if m>0 and nums[m] < nums[m-1]:
+                r = m-1
+
+            elif m < len(nums) -1 and nums[m+1]> nums[m]:
+               
+                l = m+1
+
+            else:
+                return m
+
+
+    
+
+
+
+
+
+
+
+
+        # for i in range(len(nums)):
      
-            if i == 0 and (len(nums) == 1 or nums[i] > nums[i+1]):
-                return i
+        #     if i == 0 and (len(nums) == 1 or nums[i] > nums[i+1]):
+        #         return i
           
-            if i == len(nums) - 1 and nums[i] > nums[i-1]:
-                return i
+        #     if i == len(nums) - 1 and nums[i] > nums[i-1]:
+        #         return i
        
-            if i != 0 and i + 1 < len(nums) and nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i
+        #     if i != 0 and i + 1 < len(nums) and nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+        #         return i
 
         
         
