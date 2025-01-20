c0e2bccc6068c6ac2f0305749850fd8e045879bf
Shamim Shihab <shamimshihab56@gmail.com>
2025-01-19 19:20:01 -0800

    Time: 1 ms, Memory: 54.3 MB - LeetHub

--- 17402-500-283-move-zeroes/17402-500-283-move-zeroes.ts
+++ 17402-500-283-move-zeroes/17402-500-283-move-zeroes.ts
@@ -3,43 +3,85 @@
  */
 
 
- //not a good solution
+
+//better code 
+
+
+
 function moveZeroes(nums: number[]): void {
 
-    let num  = 0
 
-    const filtere = nums.filter((n)=> {
-        
-        
-        return n!==0
-        
+    let zerod :number  =  0
+
+    for (let i = 0 ; i < nums.length ; i ++ ){
+
+        if(nums[i]!== 0 ){
+
+            let tempA =  nums[i]
+            
+            // nums[i] = 0
+
+             nums[i] = nums[zerod]
+
+
+            nums[zerod] = tempA 
+         
+
+            zerod ++
+
+
         }
-        
-        
-        
-        )
 
-    console.log(filtere)
-
-
-    let zeroneeded =  nums.length - filtere.length
-
-    for (let i=0 ; i < zeroneeded ; i ++ ){
-
-        filtere.push(0)
 
 
     }
 
-     for (let i=0 ; i < nums.length ; i ++ ){
-
-        nums[i] =  filtere[i]
 
 
-    }
 
 
 
 
     
 };
+
+ //not a good solution
+// function moveZeroes(nums: number[]): void {
+
+//     let num  = 0
+
+//     const filtere = nums.filter((n)=> {
+        
+        
+//         return n!==0
+        
+//         }
+        
+        
+        
+//         )
+
+//     console.log(filtere)
+
+
+//     let zeroneeded =  nums.length - filtere.length
+
+//     for (let i=0 ; i < zeroneeded ; i ++ ){
+
+//         filtere.push(0)
+
+
+//     }
+
+//      for (let i=0 ; i < nums.length ; i ++ ){
+
+//         nums[i] =  filtere[i]
+
+
+//     }
+
+
+
+
+    
+// };
