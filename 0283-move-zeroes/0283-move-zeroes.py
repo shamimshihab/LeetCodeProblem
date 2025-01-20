7fa4cecd20fa29925802a8e3202d7d6c0da9d912
Shamim Shihab <shamimshihab56@gmail.com>
2025-01-19 19:20:49 -0800

    Update 0283-move-zeroes.py

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -11,7 +11,37 @@ class Solution(object):
                 nums[i]= nums[j]
                 nums[j] = temporary
                 j+=1
-                
+            
+# ts solution
+# function moveZeroes(nums: number[]): void {
+
+
+#     let zerod :number  =  0
+
+#     for (let i = 0 ; i < nums.length ; i ++ ){
+
+#         if(nums[i]!== 0 ){
+
+#             let tempA =  nums[i]
+            
+#             // nums[i] = 0
+
+#              nums[i] = nums[zerod]
+
+
+#             nums[zerod] = tempA 
+         
+
+#             zerod ++
+
+
+#         }
+
+
+
+#     }
+
+
        
                 
                
